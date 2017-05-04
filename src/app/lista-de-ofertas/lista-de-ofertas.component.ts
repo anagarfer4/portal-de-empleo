4d4deb3a6aadceb89aef81070414c454b50d5493
SephorasLand <rperedah@gmail.com>
2017-05-04 09:49:36 +0200

    anadida otra oferta

--- src/app/lista-de-ofertas/lista-de-ofertas.component.ts
+++ src/app/lista-de-ofertas/lista-de-ofertas.component.ts
@@ -11,6 +11,10 @@ export class ListaDeOfertasComponent implements OnInit {
 
   oferta: Oferta = new Oferta ('Programador Junior','Lenguaje JavaScript, 4 meses de experiencia', 
     'https://lh3.googleusercontent.com/00APBMVQh3yraN704gKCeM63KzeQ-zHUi5wK6E9TjRQ26McyqYBt-zy__4i8GXDAfeys=w300', 'LinkedIn', '03/05/2017', 'Madrid')
+  oferta2: Oferta = new Oferta ('Programador Senior','Lenguaje JavaScript, 4 meses de experiencia', 
+    'https://lh3.googleusercontent.com/00APBMVQh3yraN704gKCeM63KzeQ-zHUi5wK6E9TjRQ26McyqYBt-zy__4i8GXDAfeys=w300', 'LinkedIn', '04/05/2017', 'Madrid')
+
+  
 
   constructor() { }
 
