032943322cea52a419e2dd103d93de9336197e66
Neis5 <neira_03@hotmail.com>
2017-05-10 17:51:52 +0200

    retoques de formato

--- src/app/lista-de-ofertas/oferta.ts
+++ src/app/lista-de-ofertas/oferta.ts
@@ -1,7 +1,7 @@
 export class Oferta {
 
  constructor (public nombre: string, public descripcion: string,
- public imagenUrl: string, public empresa: string, public fechaPubli: string, public ciudad: string, public jornada: string) {
+ public imagenUrl: string, public empresa: string, public fechaPubli: string, public ciudad: string, public tipoDeOferta: string, public web: string) {
 
 // hay que ampliar este constructor para que en la drescripcion aparezcan mas detalles
     }
